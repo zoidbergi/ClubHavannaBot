fc574eedb83662b637deffb53a0384a9c16f7c9c
SutterChristian <eluo@gmx.net>
2022-07-19 19:50:05 +0200

    Annoy Loe

--- src/events/eventhooks/onMessage.ts
+++ src/events/eventhooks/onMessage.ts
@@ -7,11 +7,13 @@ export const onMessageCreated = async (message: Message, client: Client) => {
     let messageContent = message.content;
 
     //stupid thing
-    /*if (message.author.id == "193383265661353984") {
-      const fivehead = client.emojis.cache.find(emoji => emoji.name === "5Head");
-      const reply: string = randomBigLetter(messageContent) + ' ' + fivehead?.toString();
-      await message.reply(reply);
-    }*/
+    if (message.author.id == "448135048021934080") {
+      if (Math.random() < 0.1) {
+        const fivehead = client.emojis.cache.find(emoji => emoji.name === "5Head");
+        const reply: string = randomBigLetter(messageContent) + ' ' + fivehead?.toString();
+        await message.reply(reply);
+      }
+    }
 
     //bot mentioned
     if (messageContent.includes('@993926727275397160')) {
