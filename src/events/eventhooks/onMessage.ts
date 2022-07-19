bf69c0d57a02179bc085dc23c46c31c2a0111094
SutterChristian <eluo@gmx.net>
2022-07-19 19:55:04 +0200

    omit slash commands in annoy function

--- src/events/eventhooks/onMessage.ts
+++ src/events/eventhooks/onMessage.ts
@@ -7,7 +7,7 @@ export const onMessageCreated = async (message: Message, client: Client) => {
     let messageContent = message.content;
 
     //stupid thing
-    if (message.author.id == "448135048021934080") {
+    if (message.author.id == "448135048021934080" && !messageContent.includes('/')) {
       if (Math.random() < 0.1) {
         const fivehead = client.emojis.cache.find(emoji => emoji.name === "5Head");
         const reply: string = randomBigLetter(messageContent) + ' ' + fivehead?.toString();
