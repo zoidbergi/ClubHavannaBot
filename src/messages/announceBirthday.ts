371689abc8dec5715588dfffeafa5170b8f573d5
SutterChristian <eluo@gmx.net>
2022-07-19 18:39:47 +0200

    fixed announcement hopefully

--- src/messages/announceBirthday.ts
+++ src/messages/announceBirthday.ts
@@ -15,11 +15,11 @@ export async function announceBirthday(client: Client) {
         if (lastAnnouncedBirthdayName !== entry.name) {
             let date = new Date(entry.birthday);
             if (isToday(date)) {
-                const channel = client.channels.cache.find(ch => ch.id === '508675397727158283');
+                const channel = client.channels.cache.find(ch => ch.id === '994584449415139418'); //994584449415139418 test channel 508675397727158283 real channel
                 if (channel?.isText()) {
                     const sendChannel = channel as TextChannel;
-                    const party = client.emojis.cache.find(emoji => emoji.name === "partying_face");
-                    const message: string = '@everyone' + ' $(entry.name) hat heute Geburtstag! WOO ' + party?.toString();
+                    const party = client.emojis.cache.find(emoji => emoji.name === "Pepega");
+                    const message: string = '@everyone' + ' ${entry.name} hat heute Geburtstag! WOO ' + party?.toString();
                     sendChannel.send(message);
                     lastAnnouncedBirthdayName = entry.name;
                 }
