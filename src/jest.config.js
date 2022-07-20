6541834c3beb35c916af4a2112af284e7111015a
SutterChristian <eluo@gmx.net>
2022-07-20 14:33:45 +0200

    fixed some jest lints

---
+++ src/jest.config.js
@@ -0,0 +1,10 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+// Sync object
+const config = {
+  verbose: true,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
+exports.default = config;
+//  # sourceMappingURL=jest.config.js.map
