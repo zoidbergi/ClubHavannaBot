9786dd9334c77a24210f7e6e71f3e9cf5fd1ce05
SutterChristian <eluo@gmx.net>
2022-07-21 13:47:06 +0200

    Fixed get next birthday feature

--- src/commands/birthday/getNextBirthday.ts
+++ src/commands/birthday/getNextBirthday.ts
@@ -6,31 +6,48 @@ import dateToString from './formatDate';
 
 const getNextBirthday: Command = {
   data: new SlashCommandBuilder()
-    .setName('nextBirthday')
+    .setName('birthdaynext')
     .setDescription('Gets next Birthday'),
   run: async (interaction) => {
     await interaction.deferReply();
 
     const result = await getBirthdayList();
 
-    if (result === undefined || result === null) {
+    if (result === undefined || result === null || result.length === 0) {
       interaction.editReply('Couldnt find shit');
       return;
     }
 
     const today = new Date();
-    let nextBirthday = { name: 'I bims the Zukunft', birthday: '10/22/3000' };
-    result.forEach((element) => {
-      const date = new Date(element.birthday);
-      if (date >= today && (date < new Date(nextBirthday.birthday))) {
-        nextBirthday = element;
+    // let nextBirthday = { name: 'niemand', birthday: '00' };
+    let nextBirthdayName = 'niemand';
+    let nextBirthdayDate = '00';
+    function isBirthdayAfterToday(birthday: Date, todayDate: Date): boolean {
+      const compare = birthday.getMonth() >= todayDate.getMonth()
+      && birthday.getDate() > todayDate.getDate();
+      console.log(compare);
+      return compare;
+    }
+
+    //  birthday list is already sorted by dd/MM so we can break on first occurrence
+    for (let i = 0; i < result.length; i += 1) {
+      const birthdayDate = new Date(result[i].birthday);
+      if (isBirthdayAfterToday(birthdayDate, today)) {
+        nextBirthdayDate = result[i].birthday;
+        nextBirthdayName = result[i].name;
+        break;
       }
-    });
+      if (i === result.length - 1 && result[0] !== undefined) {
+        //  NoBirthday found in this year. Take first list entry
+        nextBirthdayName = result[0].name;
+        nextBirthdayDate = result[0].birthday;
+      }
+    }
 
     const embedReply = new MessageEmbed();
     embedReply.setTitle('Next Brithday');
-    const readableDate = dateToString(new Date(nextBirthday.birthday));
-    embedReply.addField(nextBirthday.name, readableDate, true);
+    const readableDate = dateToString(new Date(nextBirthdayDate));
+    embedReply.addField(nextBirthdayName, readableDate, true);
     embedReply.setFooter({
       text:
         'Der isch als nägschtes dra',
